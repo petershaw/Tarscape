766ca93aab4efd2f9e29886cc02fc90b10f4b7a2
Kris Simon <kris@ausdertechnik.de>
2022-04-24 02:38:07 +0200

    Remove ObjC backport support
    Decide that this lib should be swift only

--- Sources/Tarscape/FileManager+Tar.swift
+++ Sources/Tarscape/FileManager+Tar.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 public extension FileManager {
+
+    /// Extracts the tar at `tarURL` to `dirURL`.
+    /// - Parameter at: The path of the Tar file to extract.
+    /// - Parameter to: The path to which to extract the Tar file. A directory will be created at this path containing the extracted files.
+    /// - Parameter options: List of  KBTarUnarchiver.Options, default .restoreFileAttributes
+    /// - Parameter progress: A closure with a `Double` parameter representing the current progress (from 0.0 to 1.0).
     func extractTar(at tarURL: URL, to dirURL: URL, options: KBTarUnarchiver.Options = [.restoreFileAttributes], progress: Progress? = nil) throws {
         let unarchiver = try KBTarUnarchiver(tarURL: tarURL, options: options)
         
@@ -25,6 +31,12 @@ public extension FileManager {
         try KBTarUnarchiver(tarURL: tarURL, options: options).extract(to: dirURL, progressBody: progressBody)
     }
 
+
+    /// Creates a Tar file at `tarURL`.
+    /// - Parameter at: The path at which the Tar file should be created.
+    /// - Parameter from: A directory containing the files that that should be archived.
+    /// - Parameter options: List of KBTarArchiver.Options
+    /// - Parameter progressBody: A closure with a `Double` parameter representing the current progress (from 0.0 to 1.0).
     func createTar(at tarURL: URL, from dirURL: URL, options: KBTarArchiver.Options = [],  progress: Progress? = nil) throws {
         let archiver = KBTarArchiver(directoryURL: dirURL, options: options)
         
@@ -40,28 +52,4 @@ public extension FileManager {
         try KBTarArchiver(directoryURL: dirURL, options: options).archive(to: tarURL, progressBody: progressBody)
     }
     
-    // MARK: - Objective-C Compatible Methods
-    
-    // Swift methods using OptionSet cannot be exposed to Objective-C, so we provide some alternative
-    // methods that expose the most useful options.
-    
-    /// Extracts the tar at `tarURL` to `dirURL`.
-    /// - Parameter at: The path of the Tar file to extract.
-    /// - Parameter to: The path to which to extract the Tar file. A directory will be created at this path containing the extracted files.
-    /// - Parameter restoreAttributes: If `false`, file attributes stored in the archive such as modification dates and file
-    ///     permissions be ignored. This can significantly speed up the extraction process.
-    /// - Parameter progressBody: A closure with a `Double` parameter representing the current progress (from 0.0 to 1.0).
-    func extractTar(at tarURL: URL, to dirURL: URL, restoreAttributes: Bool,  progress: Progress? = nil) throws {
-        try extractTar(at: tarURL, to: dirURL, options: restoreAttributes ? .restoreFileAttributes : [], progress: progress)
-    }
-    
-    /// Creates a Tar file at `tarURL`.
-    /// - Parameter at: The path at which the Tar file should be created.
-    /// - Parameter from: A directory containing the files that that should be archived.
-    /// - Parameter convertAliasFiles: The Tar format doesn't support alias files by default, only symbolic links. If this
-    ///     is set to `true`, archiving will check for alias files and store them as symbolic links. This can take longer.
-    /// - Parameter progressBody: A closure with a `Double` parameter representing the current progress (from 0.0 to 1.0).
-    func createTar(at tarURL: URL, from dirURL: URL, convertAliasFiles: Bool,  progress: Progress? = nil) throws {
-        try createTar(at: tarURL, from: dirURL, options: convertAliasFiles ? .convertAliasFiles : [], progress: progress)
-    }
 }
